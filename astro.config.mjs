5c474abae06f9f2acadb3315b689bb7b4385ab21
Jackson Horton <jacksonhorton12@icloud.com>
2024-09-27 11:27:49 -0500

    Started setting up astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import image from '@astrojs/image';
+
 
 export default defineConfig({
   site: 'https://oldsancboys.com',
+  integrations: [image()],
 });
