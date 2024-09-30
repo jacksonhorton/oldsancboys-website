5cbf772ddd73b33858cbdbdfee3235bd3b1a6117
Jackson Horton <jacksonhorton12@icloud.com>
2024-09-30 16:52:07 -0500

    Added music page. Added header and footer components. Added layout.

---
+++ src/components/AlbumCover.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { Img, Secondary, Heading } from "flowbite-svelte";
+
+    export let imageSrc;
+    export let albumTitle = "";
+    export let href = "";
+</script>
+
+<a href={href} class="block">
+    <Img
+        src={imageSrc}
+        alt="{albumTitle} Album Cover"
+        class="rounded-lg shadow-xl dark:shadow-gray-800 pointer-events-none mx-center"
+    />
+</a>
+
+<Heading tag="h2" customSize="text-2xl font-extrabold mt-5" class="text-center text-gray-700">{albumTitle}</Heading>
---
+++ src/components/Footer.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Footer, FooterCopyright, FooterLinkGroup, FooterLink, FooterBrand, FooterIcon } from 'flowbite-svelte';
+  import { InstagramSolid } from 'flowbite-svelte-icons';
+</script>
+
+<Footer footerType="socialmedia" class="flex-shrink-0 w-full">
+    <div class="flex flex-col py-2 rtl:space-x-reverse justify-end items-center">
+        <FooterIcon href="https://www.instagram.com/oldsancboys?igsh=MXBnZWx5MTczZHltZg%3D%3D" alt="Instagram account link">
+            <InstagramSolid class="w-6 h-6 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+        </FooterIcon>
+    </div>
+</Footer>
