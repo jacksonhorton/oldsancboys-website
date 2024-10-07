f4fe8a33608440407b713b6042c2ae933a5e3be8
jacksonhorton <jacksonhorton1212@gmail.com>
2024-10-07 09:17:34 -0500

    Added carousel image to index.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,7 +5,7 @@
 
 <Footer footerType="socialmedia" class="mt-8 flex-shrink-0 w-full">
     <div class="flex flex-row py-2 space-x-2  justify-center items-center">
-        <FooterIcon href="https://www.instagram.com/oldsancboys" alt="Instagram account link">
+        <FooterIcon target="_blank" href="https://www.instagram.com/oldsancboys" alt="Instagram account link">
             <InstagramSolid class="w-6 h-6 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
         </FooterIcon>
         <FooterIcon href="mailto:info@oldsancboys.com" alt="Email us button">
