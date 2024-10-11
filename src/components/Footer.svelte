13d050c386bfc8340bef43c55fd3b7034ac6c8da
Jackson Horton <jacksonhorton12@icloud.com>
2024-10-11 09:26:29 -0500

    Made footer anchors bigger.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,13 +3,13 @@
   import { InstagramSolid, EnvelopeOutline } from 'flowbite-svelte-icons';
 </script>
 
-<Footer footerType="socialmedia" class="mt-8 flex-shrink-0 w-full">
-    <div class="flex flex-row py-2 space-x-2  justify-center items-center">
+<Footer footerType="socialmedia" class="mt-6 flex-shrink-0 w-full">
+    <div class="flex flex-row py-2 justify-center items-center">
         <FooterIcon target="_blank" href="https://www.instagram.com/oldsancboys" alt="Instagram account link">
-            <InstagramSolid class="w-6 h-6 text-gray-500 dark:text-gray-500 hover:text-primary-600 dark:hover:text-primary-500" />
+            <InstagramSolid class="p-2 w-10 h-10 text-gray-500 dark:text-gray-500 hover:text-primary-600 dark:hover:text-primary-500" />
         </FooterIcon>
         <FooterIcon href="mailto:info@oldsancboys.com" alt="Email us button">
-            <EnvelopeOutline class="w-6 h-6 text-gray-500 dark:text-gray-500 hover:text-primary-600 dark:hover:text-primary-500" />
+            <EnvelopeOutline class="p-2 w-10 h-10 text-gray-500 dark:text-gray-500 hover:text-primary-600 dark:hover:text-primary-500" />
         </FooterIcon>
     </div>
 </Footer>
