896e9f0a63fd817ef10accab215d02e82ac32e9f
Jackson Horton <jacksonhorton12@icloud.com>
2024-10-11 08:57:58 -0500

    Updated Readme. Changed hover color on footer icons.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -6,10 +6,10 @@
 <Footer footerType="socialmedia" class="mt-8 flex-shrink-0 w-full">
     <div class="flex flex-row py-2 space-x-2  justify-center items-center">
         <FooterIcon target="_blank" href="https://www.instagram.com/oldsancboys" alt="Instagram account link">
-            <InstagramSolid class="w-6 h-6 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+            <InstagramSolid class="w-6 h-6 text-gray-500 dark:text-gray-500 hover:text-primary-600 dark:hover:text-primary-500" />
         </FooterIcon>
         <FooterIcon href="mailto:info@oldsancboys.com" alt="Email us button">
-            <EnvelopeOutline class="w-6 h-6 text-gray-500 dark:text-gray-500 hover:text-gray-900 dark:hover:text-white" />
+            <EnvelopeOutline class="w-6 h-6 text-gray-500 dark:text-gray-500 hover:text-primary-600 dark:hover:text-primary-500" />
         </FooterIcon>
     </div>
 </Footer>
