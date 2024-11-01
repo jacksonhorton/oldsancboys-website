c77e95d93cc7ed21a086c7c90ef74bf1faf2c636
Jackson Horton <jacksonhorton12@icloud.com>
2024-11-01 15:41:09 -0500

    Created basic parallax effect component.

---
+++ src/components/ParallaxBackground.svelte
@@ -0,0 +1,20 @@
+<script>
+  let scrollY
+  const parallaxImage = '/background-pattern.webp';
+</script>
+
+<svelte:window bind:scrollY={scrollY} />
+
+<!-- <h1>{scrollY}</h1> -->
+<div class="fixed parallax w-screen h-screen -z-10" style="background-position-y: {scrollY * -0.35}px; background-image: url({parallaxImage});">
+</div>
+
+<style>
+  .parallax {
+    background-image: url('/background-pattern.webp');
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center;
+    height: 100vh;
+  }
+</style>
