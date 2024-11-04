6b87ecbee8d94bf028e1f6cda1484d071bd16dc6
Jackson Horton <jacksonhorton12@icloud.com>
2024-11-04 09:53:19 -0600

    Finished parallax effect.

--- src/components/ParallaxBackground.svelte
+++ src/components/ParallaxBackground.svelte
@@ -1,12 +1,35 @@
 <script>
-  let scrollY
+  import { onMount } from 'svelte';
+
+  let scrollY, screenWidth;
   const parallaxImage = '/background-pattern.webp';
+  let multiplier = 1;
+  
+  onMount(() => {
+    multiplier = getScrollYMultiplier()
+    console.log(multiplier);
+  });
+  
+  function getScrollYMultiplier() {
+    console.log(screenWidth);
+    if (screenWidth < 768) {
+      console.log("small");
+      return -0.4; // smaller screens
+    } else if (screenWidth < 1024) {
+      console.log("med");
+      return -1.2; // mid-sized screens
+    } else {
+      console.log("large");
+      return -0.75; // Default multiplier for larger screens
+    }
+  }
+
 </script>
 
-<svelte:window bind:scrollY={scrollY} />
+<svelte:window bind:scrollY={scrollY} bind:innerWidth={screenWidth}/>
 
 <!-- <h1>{scrollY}</h1> -->
-<div class="fixed parallax w-screen h-screen -z-10" style="background-position-y: {scrollY * -0.35}px; background-image: url({parallaxImage});">
+<div class="fixed parallax w-screen h-screen -z-10" style="background-position-y: {scrollY * multiplier}px; background-image: url({parallaxImage});">
 </div>
 
 <style>
